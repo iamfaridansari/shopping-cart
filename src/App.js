79512f489efbcd0476a5a8680cab188bff3aa3bb
iamfaridansari <82579834+iamfaridansari@users.noreply.github.com>
2021-08-30 17:03:25 +0530

    Cart

--- src/App.js
+++ src/App.js
@@ -1,25 +1,172 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import "./App.css";
+import { foodItems } from "./components/dataBase";
+import { useCart } from "react-use-cart";
 
 function App() {
+  const [theme, setTheme] = useState(false);
+  useEffect(() => {
+    if (theme === true) {
+      document.body.style.background = "#212529";
+    } else {
+      document.body.style.background = "#fff";
+    }
+  }, [theme]);
+
+  const {
+    cartTotal,
+    isEmpty,
+    items,
+    totalItems,
+    addItem,
+    removeItem,
+    updateItemQuantity,
+    emptyCart,
+  } = useCart();
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      {/* navbar */}
+      <nav
+        className={
+          theme
+            ? "container bg-dark text-white p-2 rounded shadow my-2 d-flex align-items-center justify-content-between"
+            : "container bg-white p-2 rounded shadow my-2 d-flex align-items-center justify-content-between"
+        }
+      >
+        <h5 className="m-0">
+          Food Hub <i className="fas fa-home"></i>
+        </h5>
+        <div className="d-flex align-items-center justify-content-center">
+          <i
+            className={theme ? "fas fa-sun fs-5 me-2" : "fas fa-moon fs-5 me-2"}
+            onClick={() => setTheme(!theme)}
+          ></i>
+          <h5 className="m-2">
+            <span className="badge bg-primary">
+              <i className="fas fa-shopping-cart"></i>
+              {totalItems}
+            </span>
+          </h5>
+          <h5 className="m-2">
+            <span className="badge bg-primary">Total Rs.{cartTotal}</span>
+          </h5>
+        </div>
+      </nav>
+
+      {/* container */}
+      <div className="container mt-4">
+        <h1 className={theme ? "text-center text-white" : "text-center"}>
+          Products
+        </h1>
+        <div className="row justify-content-between">
+          {foodItems.map((item) => {
+            return (
+              <div
+                className={
+                  theme
+                    ? "bg-dark text-white shadow rounded col-lg-5 m-2 py-2 d-flex align-items-center justify-content-around"
+                    : "bg-white shadow rounded col-lg-5 m-2 py-2 d-flex align-items-center justify-content-around"
+                }
+                key={item.id}
+              >
+                <i className={item.img}></i>
+                <div className="details d-flex align-items-center justify-content-center flex-column">
+                  <h3>{item.name}</h3>
+                  <h5>Rs.{item.price}</h5>
+                </div>
+                <button
+                  className="btn btn-success bg-gradient"
+                  onClick={() => addItem(item)}
+                >
+                  Add To Cart
+                </button>
+              </div>
+            );
+          })}
+        </div>
+      </div>
+
+      <hr />
+
+      {/* cart */}
+      <div className="container my-4">
+        <h1 className={theme ? "text-center text-white" : "text-center"}>
+          Cart
+        </h1>
+        {isEmpty ? (
+          <h3 className={theme ? "text-center text-white" : "text-center"}>
+            No item in the cart
+          </h3>
+        ) : (
+          <>
+            <table
+              className={
+                theme ? "table table-dark table-hover" : "table table-hover"
+              }
+            >
+              <thead>
+                <tr>
+                  <th scope="col">#</th>
+                  <th scope="col">Name</th>
+                  <th scope="col">Price</th>
+                  <th scope="col">Quantity</th>
+                  <th scope="col"></th>
+                </tr>
+              </thead>
+              <tbody>
+                {items.map((item, index) => {
+                  return (
+                    <tr key={index}>
+                      <th scope="row">{index + 1}</th>
+                      <td>{item.name}</td>
+                      <td>Rs.{item.price}</td>
+                      <td>{item.quantity}</td>
+                      <td>
+                        <button
+                          className="btn btn-danger btn-sm bg-gradient"
+                          onClick={() => removeItem(item.id)}
+                        >
+                          <i className="fas fa-times"></i>
+                        </button>
+                        <div className="btn-group m-2">
+                          <button
+                            className="btn btn-warning text-white bg-gradient btn-sm"
+                            onClick={() =>
+                              updateItemQuantity(item.id, item.quantity - 1)
+                            }
+                          >
+                            <i className="fas fa-minus"></i>
+                          </button>
+                          <button
+                            className="btn btn-success bg-gradient btn-sm"
+                            onClick={() =>
+                              updateItemQuantity(item.id, item.quantity + 1)
+                            }
+                          >
+                            <i className="fas fa-plus"></i>
+                          </button>
+                        </div>
+                      </td>
+                    </tr>
+                  );
+                })}
+              </tbody>
+            </table>
+            <div className="text-center">
+              <button
+                className="btn btn-danger bg-gradient"
+                onClick={() => emptyCart()}
+              >
+                Empty Cart
+              </button>
+            </div>
+          </>
+        )}
+      </div>
+    </>
   );
 }
 
 export default App;
+// http://192.168.43.196:3000
---
+++ src/components/dataBase.js
@@ -0,0 +1,44 @@
+export let foodItems = [
+    {
+        id : 1,
+        name : "Pizza",
+        img : "fas fa-pizza-slice fs-1",
+        price : 199,
+        quantity : 1
+    },
+    {
+        id : 2,
+        name : "Burger",
+        img : "fas fa-hamburger fs-1",
+        price : 99,
+        quantity : 1
+    },
+    {
+        id : 3,
+        name : "Ice cream",
+        img : "fas fa-ice-cream fs-1",
+        price : 49,
+        quantity : 1
+    },
+    {
+        id : 4,
+        name : "Hotdog",
+        img : "fas fa-hotdog fs-1",
+        price : 149,
+        quantity : 1
+    },
+    {
+        id : 5,
+        name : "Cake",
+        img : "fas fa-cheese fs-1",
+        price : 139,
+        quantity : 1
+    },
+    {
+        id : 6,
+        name : "Waffle",
+        img : "fas fa-stroopwafel fs-1",
+        price : 249,
+        quantity : 1
+    }
+]
